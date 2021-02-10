d6af608f2476c91acd618739cbb7d3a7cb3bfa91
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-02-10 14:12:24 -0300

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MountyHall />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MountyHall from "./components/MountyHall.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MountyHall,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
